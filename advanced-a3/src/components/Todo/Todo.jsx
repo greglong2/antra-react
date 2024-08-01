cd908353971dc4d670f142f9d8481c55146d3f05
Greg Long <longgreg86@gmail.com>
2024-07-31 23:42:15 -0400

    pass todo id to editHandler

--- advanced-a3/src/components/Todo/Todo.jsx
+++ advanced-a3/src/components/Todo/Todo.jsx
@@ -7,7 +7,7 @@ export default function Todo(props) {
     return (
         <div>
             <span>{todo.title}</span>
-            <button onClick={() => { editHandler(todo) }}>Edit</button>
+            <button onClick={() => { editHandler(todo.id) }}>Edit</button>
             <button onClick={() => { deleteHandler(todo.id) }}> Delete</button>
             {
                 // <div>todo count is {count}</div> 
